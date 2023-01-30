697b07c578e40440d7a4871b69abffee4a03abbf
schiufreitas <schiufreitas@gmail.com>
2023-01-30 19:00:56 -0300

    setup

---
+++ src/main/java/valida/movimentacao/ValidaMovimentacaoApplication.java
@@ -0,0 +1,14 @@
+package valida.movimentacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidaMovimentacaoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ValidaMovimentacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/valida/movimentacao/ValidaMovimentacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package valida.movimentacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidaMovimentacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
